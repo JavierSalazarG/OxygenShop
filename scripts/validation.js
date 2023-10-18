6c41ca7a67162fcac21a4c112780f5bd4e02f574
Javi <javis9555@gm@gmail.com>
2023-10-18 20:18:10 +0200

    fix: optimize form

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,76 +1,90 @@
 const url = "https:jsonplaceholder.typicode.com/posts";
 const form_contact = document.getElementById("form_contact");
 const div__suscription = document.getElementById("div__suscription");
-
+let validFlagName = false;
+let validFlagMail = false;
 let users = [];
 
+//Validar formulario
+const validationInput = (regex, inputElement, validFlag) => {
+  if (regex.test(inputElement.value)) {
+    inputElement.style.borderBottom = "1px solid #55dfb4";
+    validFlag = true;
+  } else {
+    inputElement.style.borderBottom = "2px solid #fb3b64";
+    validFlag = false;
+  }
+  return validFlag;
+};
+
+//Limpiar campos
+const cleanFields = (nameInput, mailInput, conditionsInput, div) => {
+  nameInput.value = "";
+  mailInput.value = "";
+  conditionsInput.checked = false;
+
+  setTimeout(() => {
+    nameInput.style.borderBottom = "1px solid black";
+    mailInput.style.borderBottom = "1px solid black";
+    conditionsInput.style.border = "1px solid black";
+    div.style.display = "flex";
+  }, 300);
+  setTimeout(() => {
+    div.style.display = "none";
+  }, 3000);
+  return nameInput, mailInput, conditionsInput;
+};
+
+//Al pulsar submit ....
 form_contact.addEventListener("submit", (event) => {
   event.preventDefault();
-  const nameValidate = /^[A-Za-z]{2,100}$/;
-  const mailValidate = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,8}$/;
   const name = document.getElementById("nameInput");
   const mail = document.getElementById("mailInput");
   const conditions = document.getElementById("conditionsInput");
-  let validateU = false;
-  let validateC = false;
-  if (nameValidate.test(name.value)) {
-    validateU = true;
-    name.style.borderBottom = "1px solid #55dfb4";
-  } else {
-    validateU = false;
-    name.style.borderBottom = "2px solid #fb3b64";
-  }
-  if (mailValidate.test(mail.value)) {
-    validateC = true;
-    mail.style.borderBottom = "1px solid #55dfb4";
-  } else {
-    validateC = false;
-    mail.style.borderBottom = "2px solid #fb3b64";
-  }
+  const nameValidate = /^[A-Za-z]{2,100}$/;
+  const mailValidate = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,8}$/;
+
+  //Asigno a laidFlag el return de la funcion que es true o false
+  validFlagName = validationInput(nameValidate, name, validFlagName);
+  validFlagMail = validationInput(mailValidate, mail, validFlagMail);
+  //compruebo el check
   if (conditions.checked) {
     conditions.style.border = "1px solid #55dfb4";
   } else {
     conditions.style.border = "2px solid #fb3b64";
   }
 
-  if (validateU === true && validateC === true) {
+  if (validFlagMail && validFlagName) {
     users.push({
       name: name.value,
       email: mail.value,
     });
-    //limpiar campos
-    name.value = "";
-    mail.value = "";
-    conditions.checked = false;
+    //llamo a la API y le paso la url y users
     sendForm(url, users);
-    //ressetear campos a los 3 ms
-    setTimeout(() => {
-      name.style.borderBottom = "1px solid black";
-      mail.style.borderBottom = "1px solid black";
-      conditions.style.border = "1px solid black";
-      div__suscription.style.display = "flex";
-    }, 300);
-    setTimeout(() => {
-      div__suscription.style.display = "none";
-    }, 3000);
+    //ressetear campos a los 3 ms y limpiamos
+    cleanFields(name, mail, conditions, div__suscription);
+  } else {
+    console.log("Error al validar");
   }
 });
 
-const sendForm = async (url, date) => {
+const sendForm = (url, data) => {
   try {
-    const send = await fetch(url, {
+    const send = fetch(url, {
       method: "POST",
-      body: JSON.stringify(date),
+      body: JSON.stringify(data),
       headers: {
         "Content-type": "application/json; charset=UTF-8",
       },
-    });
-
-    if (send.status === 201) {
-      console.log(send.status);
-    } else {
-      console.log(send.status);
-    }
+    })
+      .then((response) => {
+        if (!response.ok) {
+          console.log("Error: " + send.status);
+        }
+      })
+      .then(() => {
+        console.log("OK" + response);
+      });
   } catch (error) {
     console.error(
       "Se ha producido un error al realizar la petición a la API: " + error
