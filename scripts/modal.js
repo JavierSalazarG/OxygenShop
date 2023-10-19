322f72264f2719f7fbb579d67955b2f2fe8c9e1e
Javi <javis9555@gm@gmail.com>
2023-10-19 10:38:30 +0200

    add localStorage

--- scripts/modal.js
+++ scripts/modal.js
@@ -4,23 +4,18 @@ const span__suscription = document.getElementById("span__suscription");
 const form__newsletter = document.getElementById("form__newsletter");
 const input__newsletter = document.getElementById("input__newsletter");
 
-let flag = false;
-
-/*
-Primero comprueba si lo puede mostrar con flag y luego cambia el displey a flex a los 5s
-*/
-const timeoutInit = setTimeout(() => {
-  if (!flag) {
+function markModal() {
+  localStorage.setItem("flag", "true");
+}
+function checkShown() {
+  return localStorage.getItem("flag") === "true";
+}
+if (!checkShown()) {
+  const timeoutInit = setTimeout(() => {
     background.style.display = "flex";
-    flag = true;
-  }
-}, 5000);
+  }, 5000);
 
-/*
-La funcion primero comprueba si esta activado flag, para poder mostrar, despues comprueba tu posicion de scroll y la maxima, compara y si estas por encima del 25% te lo muestra
-*/
-function checkScrollPosition() {
-  if (!flag) {
+  function checkScrollPosition() {
     const bodyHeight = document.body.offsetHeight;
     const scrollY = window.scrollY;
     const scrollPercentage = (scrollY / bodyHeight) * 100;
@@ -28,26 +23,27 @@ function checkScrollPosition() {
     if (scrollPercentage >= 25) {
       clearTimeout(timeoutInit);
       background.style.display = "flex";
-      flag = true;
     }
   }
+  window.addEventListener("scroll", checkScrollPosition);
+  //-----------------------------
 }
-window.addEventListener("scroll", checkScrollPosition);
-
-//-----------------------------
 
 //Cierre del popup al hacer click en el bonto, con la tecla esc y al tocar //fuera
 button__esc.addEventListener("click", () => {
   background.style.display = "none";
+  markModal();
 });
 document.addEventListener("keydown", (event) => {
   if (event.keyCode === 27) {
     background.style.display = "none";
+    markModal();
   }
 });
 background.addEventListener("click", (event) => {
   if (event.target === background) {
     background.style.display = "none";
+    markModal();
   }
 });
 
@@ -64,6 +60,7 @@ form__newsletter.addEventListener("submit", (event) => {
       background.style.display = "none";
       input__newsletter.value = "";
       sendForm("https:jsonplaceholder.typicode.com/posts", input__newsletter);
+      markModal();
     }, 2000);
   } else {
     input__newsletter.style.borderBottom = "2px solid #fb3b64";
